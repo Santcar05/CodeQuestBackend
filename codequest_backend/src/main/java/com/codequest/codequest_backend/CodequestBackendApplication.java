bc4602c60b9bfae0434d6b5f41063c387d99a07d
Santcar05 <castrogarzonsantiago@gmail.com>
2025-12-13 00:39:29 -0500

    inicialización de springboot

---
+++ codequest_backend/src/main/java/com/codequest/codequest_backend/CodequestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codequest.codequest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodequestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodequestBackendApplication.class, args);
+	}
+
+}
---
+++ codequest_backend/src/test/java/com/codequest/codequest_backend/CodequestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codequest.codequest_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodequestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
